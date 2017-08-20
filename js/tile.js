a22585d5a90e27a988c5a57024dd104c97d27989
lubos_000 <lubos.gelcinsky@gmail.com>
2017-08-20 12:39:16 +0200

    chore(@angular/cli): recreated application from scratch using @angular/cli

--- js/tile.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Tile = (function () {
-    function Tile() {
-    }
-    return Tile;
-}());
-exports.Tile = Tile;
-//# sourceMappingURL=tile.js.map
---
+++ src/app/playground.component.ts
@@ -0,0 +1,53 @@
+import {Component, OnInit, ViewChild} from "@angular/core";
+import {TilesComponent} from "./tiles.component"
+import {TimerComponent} from "./timer.component"
+import {CounterComponent} from "./counter.component"
+import {Tile} from "./tile";
+
+@Component ({
+    selector: "playground",
+    templateUrl: "../templates/playground.component.html",
+    styleUrls: ["../css/main.css"]
+})
+
+export class PlaygroundComponent implements OnInit {
+
+    gameInProgress : boolean;
+    ButtonLabel : string;
+
+    @ViewChild(TilesComponent)
+    private tilesComponent: TilesComponent;
+    @ViewChild(TimerComponent)
+    private timerComponent: TimerComponent;
+    @ViewChild(CounterComponent)
+    private counterComponent: CounterComponent;
+
+    counter : number = 0;
+    countDown : number = 3;
+
+    constructor () {};
+
+    ngOnInit() : void {
+        this.gameInProgress = false;
+        this.ButtonLabel = "Start";
+    }
+    
+    start() {
+        this.tilesComponent.start();
+        this.timerComponent.start();
+        this.counterComponent.start();
+        this.gameInProgress = true;    
+    }    
+
+    onTimerExpired() {
+        this.gameInProgress = false;
+    }
+
+    onTileClicked(tile : Tile) {
+        if (tile.active && !this.timerComponent.isTimeExpired()) {
+            this.tilesComponent.setAnotherTile(tile);
+            this.counterComponent.raiseCounter();
+        }
+    }
+
+}
